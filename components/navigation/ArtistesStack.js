92364a66a7b09036fb0f3fffb827bb04632d0d07
Zozo <benbar2412@gmail.com>
2023-07-22 13:51:14 +0200

    MAJ indentation ArtistesStack

--- components/navigation/ArtistesStack.js
+++ components/navigation/ArtistesStack.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { DetailsArtiste, Programmation}  from "../../components";
+import { DetailsArtiste, Programmation } from "../../components";
 
 const Stack = createStackNavigator();
 
 const ArtistesStack = ({ artistes }) => {
-    return (
-        <Stack.Navigator initialRouteName="Programmation" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Programmation">{(props) => <Programmation {...props} artistes={artistes} />}</Stack.Screen>
-            <Stack.Screen name="Details de l'artiste" component={DetailsArtiste} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      initialRouteName="Programmation" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Programmation">{(props) => <Programmation {...props} artistes={artistes} />}</Stack.Screen>
+      <Stack.Screen name="Details de l'artiste" component={DetailsArtiste} />
+    </Stack.Navigator>
+  );
 };
 
 export default ArtistesStack;
