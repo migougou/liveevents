c447da644ed77a73d64463ed15bb414deaf29c2c
Zozo <benbar2412@gmail.com>
2023-09-29 19:30:04 +0200

    post finition terminé

--- apiflasksqlalchemyMTM/server/api/resources/LocalisationsRessource.py
+++ apiflasksqlalchemyMTM/server/api/resources/LocalisationsRessource.py
@@ -4,32 +4,43 @@ from orm.controller.LocalisationsController import LocalisationsController
 # Classe pour la ressource Clients, dérivée de la ressource générique
 class LocalisationsResource(GenericResource):
 
-    def __init__(self):
-        '''
-        Constructeur
-        '''
-        # Initialisation de la ressource générique avec le contrôleur
-        GenericResource.__init__(self, serviceController=LocalisationsController())
-        # Initialisation du contrôleur de service
-        self.serviceController.__init__()
+  def __init__(self):
+    # Initialisation de la ressource générique avec le contrôleur
+    GenericResource.__init__(self, serviceController=LocalisationsController())
 
-    # Méthode GET pour récupérer les localisations
-    def get(self):
-        res = []
-        localisations = self.serviceController.selectLocalisations()
-        # Si aucun localisation n'est trouvé, renvoie un message d'erreur
-        if localisations is None or localisations == []:
-            return {'message': 'Localisations not found'}, 404
-        else:
-            # Sinon, transforme chaque localisation en dictionnaire et l'ajoute à la liste des résultats
-            for l in localisations:
-                res.append(l.to_dict())
-        return res, 200
+    # Initialisation du contrôleur de service
+    self.serviceController.__init__()
 
-    # Méthode POST pour ajouter un nouvel client
-    def post(self):
-        data = self.rebuild_params()
-        res = []
+  def get(self):
+    localisations = self.serviceController.selectLocalisations()
+    if not localisations: return {'message': 'Localisations not found'}, 404
 
-        print(data)
-        return data
+    data = []
+    for l in localisations:
+      data.append(l.to_dict())
+    
+    return data, 200
+
+  def post(self):
+    data = self.rebuild_params()
+
+    return self.transform_data(data)
+
+  def transform_data(self, input_data):
+    """
+    Transforme les données d'entrée en un format spécifique en extrayant les informations essentielles.
+    """
+    output_data = []
+
+    for item in input_data:
+      location_data = {
+        "id": item["id"],
+        "localisation": item["acf"]["localisation"],
+        "lattitude": item["acf"]["latitude"],
+        "longitude": item["acf"]["longitude"],
+        "description": item["acf"]["description"]
+      }
+
+      output_data.append(location_data)
+
+    return output_data
