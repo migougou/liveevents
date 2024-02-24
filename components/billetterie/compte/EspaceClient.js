5cd0c60c113cedfa09c1f4e42344ff369b678351
CorentinCLERO <corentin.clero@epsi.fr>
2024-02-24 13:06:55 +0100

    resolve useState error

--- components/billetterie/compte/EspaceClient.js
+++ components/billetterie/compte/EspaceClient.js
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Text, View } from 'react-native';
-import { deleteInfoClient, storeData } from '../../utilities';
+import { deleteInfoClient } from '../../utilities';
 import axios from 'axios';
 
 const EspaceClient = ({ infosClient, setInfosClient, ordersData }) => {
-  const [ordersClient, setOrdersClient] = useState(null)
+  const [commandeClient, setCommandeClient] = useState([])
 
   useEffect(() => {
     axios.get(`http://cchost.freeboxos.fr:5001/clients/${infosClient?.id}`)
-    .then(response => {
-      setInfosClient(response.data)
-      storeData(response.data, "infosClient")
-    })
-    .catch(error => {
-      console.log(error)
-    })
+      .then(response => {
+        setInfosClient(response.data)
+        storeData(data, "infosClient")
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }, [])
+
+  console.log('ordersData', ordersData)
+  console.log('infosClient', infosClient)
+  console.log('commandeClient', commandeClient)
+
+  useEffect(() => {
+    const commande = ordersData?.filter(order => infosClient?.commandes.includes(order.id));
+    setCommandeClient(commande);
+  }, [infosClient]);
+  
+
   return (
     <View>
       <View>
@@ -29,6 +40,7 @@ const EspaceClient = ({ infosClient, setInfosClient, ordersData }) => {
       <Text>Pays : {infosClient.pays}</Text>
       <Text>Telephone : {infosClient.telephone}</Text>
       <Text>Email : {infosClient.email}</Text>
+      {commandeClient?.map(commande => <Text key={commande.id}>Prix commande : {commande.total}</Text>)}
     </View>
   );
 };
